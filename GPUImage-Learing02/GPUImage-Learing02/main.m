4c26fa36bf6bdde9f977b8beb0e12f48608f6870
郝高明 <978235424@qq.com>
2017-10-18 15:51:46 +0800

    pod 添加GPUImage
    pod 添加GPUImage

---
+++ GPUImage-Learing02/GPUImage-Learing02/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GPUImage-Learing02
+//
+//  Created by haogaoming on 2017/10/18.
+//  Copyright © 2017年 郝高明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
